7e52b0cbbe35eba44c9a896756fb1b35a3187748
hexc <hexc@fujica.com.cn>
2024-01-30 16:46:45 +0800

    feat(Format-money): 金额转换配置

--- src/Format.ts
+++ src/Format.ts
@@ -1,19 +1,41 @@
+import { Calc } from "./Calc"
 /**
  * @description
  * <span style='color:red;font-weight:bold'>格式化某个数据</span>
  * |方法名|参数|输出
  * |---|---|---|
- * |Format.money|(money金额本身, fenToYuan是否分转元, affix前缀)|Number
+ * |Format.money|(money金额本身, config配置)|number
  * @version 1.0.0
  */
-import { Calc } from "./Calc"
 export const Format = {
-  money: (money, fenToYuan, affix = '') => {
-    if (fenToYuan) money = Calc.divide(money, 1000)
-    if (affix) {
-      return affix + money
+  /**
+   * 金额转换
+   * @param money 金额
+   * @param config 配置
+   * @returns
+   */
+  money: (money: number, config: moneyConfig) => {
+    if (config.fenToYuan) money = Calc.divide(money, 1000)
+    if (config.precision || config.precision === 0) {
+      money.toFixed(config.precision)
+    }
+    if (config.affix) {
+      return config.affix + String(money)
     } else {
       return money
     }
   }
+}
+
+/**
+ * 金额转换配置
+ * @interface
+ */
+export interface moneyConfig {
+  /** 保留几位小数 */
+  precision?: number,
+  /** 是否分转元 */
+  fenToYuan?: boolean,
+  /** 前缀 */
+  affix?: string,
 }
