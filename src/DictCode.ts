1274fab702fd5b37055c6666777699c2c303bcf4
hexc <hexc@fujica.com.cn>
2024-02-03 11:09:58 +0800

    feat(src): DICT_CODE字典编码集合

---
+++ src/DictCode.ts
@@ -0,0 +1,84 @@
+/**
+ * @description
+ * <span style='color:red;font-weight:bold'>字典编码集合</span>
+ * |方法名|输出|备注
+ * |---|---|---|
+ * |DICT_CODE.public_cartype|'216'|车牌颜色
+ * @version 1.0.0
+ */
+export const DICT_CODE = {
+  /** 出入类型 */
+  inout_type: '112',
+  /** 通行凭证类型 */
+  voucher_type: '131',
+  /** 凭证状态 */
+  license_status: '132',
+  /** 卡类别 */
+  card_type: '133',
+  /** 四个凭证通用状态 */
+  public_license_status: '134',
+  /** 授权类型 */
+  auth_type: '136',
+  /** 审批用户类型 */
+  customer_type: '137',
+  /** 凭证类型 */
+  license_type: '138',
+  /** 授权状态 */
+  auth_status: '139',
+  /** 卡状态 */
+  card_status: '143',
+  /** 证件类型 */
+  identity_type: '173',
+  /** 学历 */
+  education_type: '175',
+  /** 梯控设备状态 */
+  device_state: '182',
+  /** 菜单管理-系统类型 */
+  menusystem_type: '183',
+  /** 异常类型 */
+  abnormal_type: '185',
+  /** 记录类型 */
+  record_type: '186',
+  /** 报警事件类型 */
+  alarm_type: '188',
+  /** 报警处理设备类型 */
+  devicename_type: '201',
+  /** 人行车牌颜色 */
+  p_plate_color_type: '212',
+  /** 人行车类型 (暂无) */
+  p_car_type: '213',
+  /** 车牌颜色 */
+  public_cartype: '216',
+  /** 访客证件类型 */
+  id_type: '218',
+  /** 卡凭证类型 */
+  cardvoucher_type: '237',
+  /** 梯控类型 */
+  elevator_type: '219',
+  /** 梯控脱离原因 */
+  elevator_reason: '220',
+  /** 梯控设备品牌 */
+  device_brand: '221',
+  /** 梯控监控设备类型 */
+  monitor_device_type: '221',
+  /** 开票平台类型 */
+  platform_type: '226',
+  /** 通行时间段类型 */
+  schedule_type: '229',
+  /** 处理方式 */
+  handle_method: '244',
+  /** 通行时间段类型 */
+  ops_type: '259',
+  /** 支付方式 */
+  pay_way: '284',
+  /** 支付方式 */
+  pay_type: '284',
+  /** 支付类型 */
+  pay_type1: '211',
+  /** 上海地标支付类型 */
+  pay_type_db: '249',
+  /** 支付渠道 */
+  pay_channel: '280',
+  /** 支付来源 */
+  pay_source: '281'
+}
