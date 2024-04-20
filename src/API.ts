0e433101d1ced0711797e361ecddd919e0fa84cf
hexc <hexc@fujica.com.cn>
2024-04-20 17:42:42 +0800

    docs: API USAGE

--- src/API.ts
+++ src/API.ts
@@ -3,6 +3,16 @@ import API from './sdk/index'
  * @category API
  * @description
  * <span style='color:red;font-weight:bold'>API</span>
+ * 使用前需要在项目的请求文件中引入进行配置
+ * ```js
+ *  import { API } from "@fujica/utils"
+ *  API.setStore({
+ *    config: {
+ *      baseURL: baseURL,
+ *      tokenKey: "xxx-Token",
+ *    },
+ *  })
+ *   ```
  * 方法名|输入|输出|备注
  * ---|---|---|---
  * API.getDict|'284'|[]|通过pid获取字典
